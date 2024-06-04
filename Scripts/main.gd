1f567088bdf9b7eec24d95a99c6b5d636c860c5d
RobertAj0857 <robertaj0857@gmail.com>
2024-06-04 23:48:56 +0200

    int_from_float

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,17 +1,6 @@
 extends Node
 
-var health = 100
 
-# Called when the node enters the scene tree for the first time.
-func _input(event):
-	if event.is_action_pressed("MyAction"):
-		health -= 20
-		print(health)
-		
-		if health <= 0:
-			health = 0
-			print("YOU DIED")
-		elif health < 50: 
-			print("YOU ARE INJURED")
-		else:
-			print("YOU ARE HEALTHY")
+func _ready():
+	var pi = 3.14
+	print(int(pi))
