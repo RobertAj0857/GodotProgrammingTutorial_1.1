7fbb2a42989d99bd62b0af9fddc846bf9b49a297
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 01:16:07 +0200

    Dictionaries 1.0
    The Rock rocks my rocky rock road

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,14 +1,14 @@
 extends Node
 
 func _ready():
-	var glass := 0.0
+	var players = {
+		"Crook": 1, 
+		"Villain": 35, 
+		"Boss": 100
+	}
 	
-	while glass < 0.5:
-		glass += randf_range(0.01, 0.2)
-		
-		if glass > 0.2:
-			break
-		
-		print(glass)
+	players["Villain"] = 50
+	players["Dwayne (The Rock) Johnson"] = 999
 	
-	print("The glass is now half full!")
+	for username in players:
+		print(username + ": " + str(players[username]))
