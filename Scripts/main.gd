6f618af0b262e80751fdbf52defcf9b82c04a4ed
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 00:41:34 +0200

    Height check
    Find height with randf and display with print

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,8 +1,5 @@
 extends Node
 
 func _ready():
-	var roll = randf()
-	if roll <= 0.8:
-		print("You found a COMMON item")
-	else:
-		print("You found a RARE item")
+	var character_height = randf_range(140, 210)
+	print("Your character is " + str(character_height) + "cm Tall.")
