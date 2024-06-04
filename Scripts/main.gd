63df846cdf8921a4d152c34cdd9b9d71cdb097a0
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 01:00:18 +0200

    for loop 1.1
    n is the only thing that keeps me going

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,8 +1,6 @@
 extends Node
 
 func _ready():
-	var items: Array[String] = ["Potion", "Rapier with dried blood on the tip", "Head"]
 	
-	for item in items:
-		if item.length() > 6:
-			print(item)
+	for n in 8:
+		print(n)
