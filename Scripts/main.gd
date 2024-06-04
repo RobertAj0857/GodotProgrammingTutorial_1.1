bb5aae647f14c4c28e409913b01dbba76b94736a
RobertAj0857 <robertaj0857@gmail.com>
2024-06-04 23:36:08 +0200

    Health_Input
    Using inputs and if - else as health indicators

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,12 +3,15 @@ extends Node
 var health = 100
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	health = 40 # assign to value
-	health = 20 + 30 # assign to calculation
-	health += 20 # add
-	health -= 10 # subtract
-	health *= 4 # multiply
-	health /= 2 # divide
-	
-	print(health)
+func _input(event):
+	if event.is_action_pressed("MyAction"):
+		health -= 20
+		print(health)
+		
+		if health <= 0:
+			health = 0
+			print("YOU DIED")
+		elif health < 50: 
+			print("YOU ARE INJURED")
+		else:
+			print("YOU ARE HEALTHY")
