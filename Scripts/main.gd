4f7e719e83f7b4f1ab15aaaeb8a3a2dcf015d6f7
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 01:07:01 +0200

    For loop 1.2
    The Glass is an illusion. It never existed

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,14 @@
 extends Node
 
 func _ready():
+	var glass := 0.0
 	
-	for n in 8:
-		print(n)
+	while glass < 0.5:
+		glass += randf_range(0.01, 0.2)
+		
+		if glass > 0.2:
+			break
+		
+		print(glass)
+	
+	print("The glass is now half full!")
