878e34e2ff0d208705f2083e53efc98e06a20423
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 00:37:31 +0200

    Item_Finder
    First Time = RARE
    Get shit on liberals *puts on sunglasses*

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,8 @@
 extends Node
 
 func _ready():
-	var result = add(3, 5)
-	result = add(result, 10)
-	print(result)
-
-
-func add(num1, num2):
-	var result = num1 + num2
-	return result
+	var roll = randf()
+	if roll <= 0.8:
+		print("You found a COMMON item")
+	else:
+		print("You found a RARE item")
