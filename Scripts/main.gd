3088b3a6feb691f148a26e3010e4910dacd47d2f
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 14:01:26 +0200

    Signals 1.1
    Connect a signal from main.gd to main.gd through the _ready func

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,10 +1,19 @@
 extends Node
 
+signal leveled_up(msg)
 
+var xp := 0
 
 func _ready():
-	pass
+	leveled_up.connect(_on_leveled_up)
+
+func _on_timer_timeout():
+	xp += 5
+	print(xp)
+	if xp >= 20:
+		xp = 0
+		leveled_up.emit("RATS")
 
 
-func _on_button_pressed():
-	print("MONEY")
+func _on_leveled_up(msg):
+	print(msg)
