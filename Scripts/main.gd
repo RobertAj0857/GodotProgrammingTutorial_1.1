0334a2922789ee948ef198219d713a3e81da1522
RobertAj0857 <robertaj0857@gmail.com>
2024-06-05 13:13:14 +0200

    Enum backstory
    Description on how them enums be made

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,8 @@
 extends Node
 
-enum Alignment { ALLY, NEUTRAL, ENEMY }
+enum Alignment { ALLY = 1, NEUTRAL = 0, ENEMY = -1 }
 
 @export var unit_alignment = Alignment.ALLY
 
 func _ready():
-	if unit_alignment == Alignment.ENEMY:
-		print("You are not welcome here.")
-	else:
-		print("Welcome!")
+	print(Alignment.ENEMY)
